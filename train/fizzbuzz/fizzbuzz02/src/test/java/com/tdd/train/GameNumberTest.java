52827ea0b3bc201b5032413b6085efccfc39331b
lufeil6 <907617972@qq.com>
2019-08-11 20:50:55 +0800

    02train-13min

---
+++ train/fizzbuzz/fizzbuzz02/src/test/java/com/tdd/train/GameNumberTest.java
@@ -0,0 +1,42 @@
+package com.tdd.train;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class GameNumberTest {
+
+    @Test
+    public void should_show_raw_number_if_normal_number() {
+        checkGameNumberOutput(1, "1");
+        checkGameNumberOutput(2, "2");
+    }
+
+    @Test
+    public void should_show_fizz_if_raw_number_is_divisible_by_3() {
+        checkGameNumberOutput(3, "fizz");
+    }
+
+    @Test
+    public void should_show_buzz_if_raw_number_is_divisible_by_5() {
+        checkGameNumberOutput(5, "buzz");
+    }
+
+    @Test
+    public void should_show_fizzbuzz_if_raw_number_is_divisible_by_3_and_5() {
+        checkGameNumberOutput(15, "fizzbuzz");
+    }
+
+    @Test
+    public void should_show_fizz_or_buzz_if_raw_number_contains_3_or_5() {
+        checkGameNumberOutput(13, "fizz");
+        checkGameNumberOutput(25, "buzz");
+        checkGameNumberOutput(51, "fizzbuzz");
+        checkGameNumberOutput(35, "fizzbuzz");
+    }
+
+    private void checkGameNumberOutput(int i, String s) {
+        assertThat(new GameNumber(i).toString(), is(s));
+    }
+}
