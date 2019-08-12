5b707d2c83170d98589a48e51c70b03a93eb3ccb
lufeil6 <907617972@qq.com>
2019-08-12 21:21:15 +0800

    02train-10min

---
+++ train/fizzbuzz/fizzbuzz03/src/main/java/com/tdd/train/GameNumber.java
@@ -0,0 +1,26 @@
+package com.tdd.train;
+
+import static java.lang.String.valueOf;
+
+public class GameNumber {
+    private int rawNumber;
+
+    public GameNumber(int rawNumber) {
+        this.rawNumber = rawNumber;
+    }
+
+    @Override
+    public String toString() {
+        if (isRelatedTo(3) && isRelatedTo(5))
+            return "fizzbuzz";
+        if (isRelatedTo(3))
+            return "fizz";
+        if (isRelatedTo(5))
+            return "buzz";
+        return valueOf(rawNumber);
+    }
+
+    private boolean isRelatedTo(int i) {
+        return rawNumber % i == 0 || valueOf(rawNumber).contains(valueOf(i));
+    }
+}
